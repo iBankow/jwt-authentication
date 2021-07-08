0c7604a169d4532b90db3dac29a7be2f2ad8eabd
Tiago Bankow <bkw.designer@gmail.com>
2021-07-08 17:56:53 -0400

    .

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,16 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  username: string;
 
-    @Column()
-    firstName: string;
+  @Column()
+  email: string;
 
-    @Column()
-    lastName: string;
-
-    @Column()
-    age: number;
-
+  @Column()
+  password: number;
 }
